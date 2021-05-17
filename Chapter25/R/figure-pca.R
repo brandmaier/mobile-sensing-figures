879e359bc5b3a477da14bebbea3fea86897af59e
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2021-05-17 11:47:56 +0200

    fixed PCA labels

--- Chapter25/R/figure-pca.R
+++ Chapter25/R/figure-pca.R
@@ -1,6 +1,8 @@
 #
 # devtools::install_github("vqv/ggbiplot")
 #
+#if (!exists("mytheme"))
+mytheme <- papaja::theme_apa()
 
 accelerometer.data <- read.csv("../data/accelerometer-all.dat")
 head(accelerometer.data)
@@ -21,10 +23,6 @@ labels[which(labels %in% "Clapping")] <- "Clapping hands"
 labels[which(labels %in% "Brushing")] <- "Brushing teeth"
 
 labels <- as.factor(as.character(labels))
-#my.dat <- rbind(walking.data, soup.data, brush.data)
-#labels <- c(rep("Walk",nrow(walking.data)),rep("Soup",nrow(soup.data)), rep("Brush",nrow(brush.data))  )
-
-#my.dat <- my.dat[sample(1:nrow(my.dat)),]
 
 my.dat <- apply(my.dat, 2, scale)
 
@@ -39,11 +37,7 @@ dims <- length(idxs)
 #
 eg <- stats::prcomp(my.dat)
 library(ggbiplot)
-g2 <- ggbiplot(eg,alpha=0)+theme_minimal()+
-  geom_point(size=3,aes(shape=labels))+
-  xlim(-2,2.8)+
-  scale_shape(name="Activities")+guides(color=FALSE)+
-  scale_color_grey()
+
 
 #
 # compute ICA
@@ -52,11 +46,15 @@ egica <- ica::icaimax(my.dat,2)
 egica <- ica::icafast(my.dat, nc=2)
 g3 <- ggplot(data.frame(egica$S,labels))+
   geom_point(aes(x=X1,y=X2,col=labels,shape=labels))+
-  theme_minimal()
+  mytheme
+
+
+# plot
+
+g2<- ggbiplot(eg,alpha=0,groups=labels,obs.scale=.2,var.scale=.2)+theme_minimal()+
+  geom_point(size=3,aes(shape=labels))+
+ # xlim(-2,2.8)
+  scale_shape(name="Activities")+guides(color=FALSE)+
+  scale_color_grey()+mytheme
 
 plot(g2)
-
-
-#library(patchwork)
-
-#g2 | g3
