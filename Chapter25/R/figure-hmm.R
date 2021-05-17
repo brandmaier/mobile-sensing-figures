427e12399c9ba14a5761fb7b3f57fd838e75c6d1
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2021-05-17 11:56:13 +0200

    added comments to code

--- Chapter25/R/figure-hmm.R
+++ Chapter25/R/figure-hmm.R
@@ -1,24 +1,31 @@
-# mostly from here: https://www.r-bloggers.com/hidden-markov-model-example-in-r-with-the-depmixs4-package/
+#
+# this code is inspired from this fantastic resource here: 
+# https://www.r-bloggers.com/hidden-markov-model-example-in-r-with-the-depmixs4-package/
+#
 
+# load libraries
 library(depmixS4)
 library(ggplot2)
 library(gridExtra)
 library(reshape2)
 library(dplyr)
 
+# set random seed for reproducibility
 set.seed(124)
-#set.seed(3294)
 
+# set labels for the two activities
 state1.name <- "Walking"
 state2.name <- "Sitting"
 
+# set sample size
 N <- 200
 x <- rnorm(N)
 y <- rnorm(N)
 
-mycols <- c("darkmagenta", "turquoise")
+#mycols <- c("darkmagenta", "turquoise")
 mycols <- c("#5270B3","#FF7052")
 
+# data generating process: jump between two states with chance of 10%
 true.states <- rep(state1.name,N)
 cur <- state1.name
 for (i in 2:N) {
@@ -28,23 +35,17 @@ for (i in 2:N) {
   true.states[i] <- cur
 }
 
+# add offset to observations depending on state
 x[which(true.states==state1.name)] <- x[which(true.states==state1.name)] + 2
 
 draws <- data.frame(Observed=x, state=true.states, time=1:N)
 
-#mod <- depmix(obs+y ~ 1, data = draws, nstates = 2, family = gaussian()) # use gaussian() for normally distributed data
-#fit.mod <- fit(mod)
-
-
-
-#est.states <- posterior(fit.mod)
-#head(est.states)
-#read.csv("~/Documents/")
-
+# define function to fit the HMM
 fit.hmm <- function(draws){
   
   # HMM with depmix
-  mod <- depmix(Observed ~ 1, data = draws, nstates = 2, family = gaussian()) # use gaussian() for normally distributed data
+  mod <- depmix(Observed ~ 1, data = draws, 
+                nstates = 2, family = gaussian()) # use gaussian() for normally distributed data
   fit.mod <- fit(mod)
   
   # predict the states by estimating the posterior
@@ -92,7 +93,7 @@ plot.hmm.output <- function(model.output){
            labs(y = "Posterior")) %>%
     ggplotGrob()
   g0$widths <- g1$widths
-#  return(grid.arrange(g0, g1, g2, g3, widths = 1, nrow = 4))
+
   return(grid.arrange(g0, g1,  g3, widths = 1, nrow = 3))
 }
 
